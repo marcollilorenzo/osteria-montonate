42fdedd7f6161300b2ff947fb6715aa39b498a66
Lorenzo Marcolli <lory.marcolli@gmail.com>
2022-01-28 12:21:13 +0100

    add alt image

--- src/components/MenuCard.jsx
+++ src/components/MenuCard.jsx
@@ -5,10 +5,10 @@ function MenuCard({ data }) {
     const tooltip = useRef()
 
     return (
-        <div className='bg-white shadow-lg rounded-lg m-4 md:m-6'>
+        <article className='bg-white shadow-lg rounded-lg m-4 md:m-6'>
             <div className='relative rounded-lg overflow-hidden group'>
                 <div className='group-hover:scale-105  absolute object-cover h-[20vh] w-full rounded-lg bg-black opacity-30'></div>
-                <img src={data.image} className=' duration-700 transition group-hover:scale-105 object-cover h-[20vh] w-full rounded-lg' />
+                <img src={data.image} alt={data.categoryName} className=' duration-700 transition group-hover:scale-105 object-cover h-[20vh] w-full rounded-lg' />
                 <h2 className='top-1/2 left-1/2 -translate-x-1/2  -translate-y-1/2 absolute font-bold text-4xl py-4 text-center text-white'>{data.categoryName}</h2>
 
             </div>
@@ -28,7 +28,7 @@ function MenuCard({ data }) {
                                                 return (
                                                     <div className='relative group' ref={tooltip}>
                                                         <p className='absolute bottom-8 bg-osteriaRed text-white p-2 rounded-lg hidden group-hover:block'>{allergen.name}</p>
-                                                        <img className='h-6 w-6 cursor-pointer' src={allergen.image} />
+                                                        <img className='h-6 w-6 cursor-pointer' alt={allergen.image} src={allergen.image} />
                                                     </div>
                                                 )
                                             })
@@ -42,7 +42,7 @@ function MenuCard({ data }) {
                     )
                 })
             }
-        </div>
+        </article>
     );
 }
 
